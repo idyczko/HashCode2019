2767b37e04e8c016a08f3956e8ae79d1eb3459e6
Dyczko Igor (IT-SWE-CC1-JV6) <igor.dyczko@sbb.ch>
2019-02-28 10:26:34 +0100

    Really up to date pizza and filled template for Qualification Round.

--- qround/Main.java
+++ qround/Main.java
@@ -1,7 +1,37 @@
 
 public class Main {
 
+	private static final String TAG = "///////////////////////////////////// %s /////////////////////////////////////";
+	private static final String RESULT_FILENAME = "result.out";
+	private static boolean log = false;
+  private static boolean saveResult = false;
+	private static int GRAN;
+	private static String stats = "";
+
 	public static void main(String[] args) {
-	
+		log |= option(args, "-l");
+    concurrent |= option(args, "-c");
+    saveResult |= option(args, "-s");
+		GRAN = checkValue(args, "-gran", 1);
+
+		Scanner in = new Scanner(System.in);
+
 	}
+
+	/*
+	private static void saveResultToFile(Set<Slice> expansionSolution) {
+    Path path = Paths.get(".", RESULT_FILENAME);
+    List<String> results = expansionSolution.stream().map(Slice::toSaveString).collect(Collectors.toList());
+    results.add(0, String.valueOf(expansionSolution.size()));
+    try {
+      Files.write(path, results);
+    } catch (IOException e){
+      System.out.println(e.getMessage());
+    }
+  }*/
+
+	private static void log(String str) {
+    if (log)
+      System.out.println(str);
+  }
 }
